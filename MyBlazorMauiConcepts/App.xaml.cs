4d1f4197d11fd786b1c55e82918b36c6d9037a0f
iabdel <isam.abdelhameed@outlook.com>
2023-03-09 12:21:41 +0300

    init

---
+++ MyBlazorMauiConcepts/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBlazorMauiConcepts;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MyBlazorMauiConcepts/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBlazorMauiConcepts;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
